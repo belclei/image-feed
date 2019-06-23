3320d89cfb399319eada5508de4478fbcb8eb9a5
belclei <belclei@gmail.com>
2019-06-23 11:22:53 -0300

    It was built CardList component

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 import { Constants } from "expo";
+
+const items = [
+  { id: 0, author: "Chuck Norris" },
+  { id: 1, author: "John Snow" },
+  { id: 2, author: "Belclei Fasolo" },
+  { id: 3, author: "Sabrina de Freitas" },
+  { id: 4, author: "Selia Fasolo" },
+  { id: 5, author: "Gabriele Fasolo" },
+  { id: 6, author: "Helio Fasolo" },
+  { id: 7, author: "Antonio Nunes" },
+  { id: 8, author: "Armando Volta" }
+];
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Card
-          fullname={"Belclei Fasolo"}
-          linkText={"6 Comentários"}
-          onPressLinkText={() => {
-            console.log("link pressed!");
-          }}
-          image={{ uri: "https://unsplash.it/600/600" }}
-        />
+        <CardList items={items} />
       </View>
     );
   }
